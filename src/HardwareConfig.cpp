09af47b5084e1a8eaf88a23862f17e39124f2af4
Louis Cormier <louiscormier1@gmail.com>
2024-02-05 13:00:42 -0500

    add pwm extremum values in config

--- src/HardwareConfig.cpp
+++ src/HardwareConfig.cpp
@@ -6,4 +6,6 @@ const int HardwareConfig::MODE_BUTTON_PIN = 2;
 const int HardwareConfig::SENSOR_PIN = 1;
 const int HardwareConfig::ANALOG_INPUT_MIN_VALUE = 0;
 const int HardwareConfig::ANALOG_INPUT_MAX_VALUE = 1023;
+const int HardwareConfig::PWM_MIN_VALUE = 0;
+const int HardwareConfig::PWM_MAX_VALUE = 255;
 const double HardwareConfig::OPERATING_VOLTAGE = 5.0;
