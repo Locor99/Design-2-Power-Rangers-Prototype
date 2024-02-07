2ac19558073ea0aa05b73b61d6a9d9370529baa1
Louis Cormier <louiscormier1@gmail.com>
2024-02-07 11:21:08 -0500

    divide parameters in different classes

---
+++ include/hardware_config.h
@@ -0,0 +1,29 @@
+#ifndef HARDWARECONFIG_H
+#define HARDWARECONFIG_H
+#include "Arduino.h"
+
+class ArduinoConfig {
+public:
+    static const double OPERATING_VOLTAGE;
+    static const int SENSOR_PIN;
+    static const int MODE_BUTTON_PIN;
+    static const int ACTUATOR_PIN;
+    static const int ANALOG_INPUT_MIN_VALUE;
+    static const int ANALOG_INPUT_MAX_VALUE;
+    static const int PWM_MIN_VALUE;
+    static const int PWM_MAX_VALUE;
+};
+
+class DistanceSensorConfig {
+public:
+    static const double MIN_READABLE_DISTANCE_MM;
+    static const double MAX_READABLE_DISTANCE_MM;
+};
+
+class ActuatorConfig {
+public:
+    static const double MIN_FORCE_N;
+    static const double MAX_FORCE_N;
+};
+
+#endif // HARDWARECONFIG_H
