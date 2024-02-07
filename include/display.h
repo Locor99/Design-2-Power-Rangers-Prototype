d078cb15825fd818a406b3bc44ca7d816062ad4b
Louis Cormier <louiscormier1@gmail.com>
2024-02-07 17:03:20 -0500

    display class

---
+++ include/display.h
@@ -0,0 +1,16 @@
+#ifndef DESIGN2_PROTOTYPE_DISPLAY_H
+#define DESIGN2_PROTOTYPE_DISPLAY_H
+
+#include "LiquidCrystal.h"
+#include "hardware_config.h"
+#include "Arduino.h"
+
+class Display {
+public:
+    Display(LiquidCrystal &lcd);
+    void displayMass(double mass);
+private:
+    LiquidCrystal _lcd;
+};
+
+#endif //DESIGN2_PROTOTYPE_DISPLAY_H
