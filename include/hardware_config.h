d92ef5f94677ae6d4c16041f0f36bd87d060ffe6
Louis Cormier <louiscormier1@gmail.com>
2024-02-07 19:49:52 -0500

    lcd screen config

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -33,6 +33,17 @@ private:
     ActuatorConfig();
 };
 
+class LcdScreenConfig{
+public:
+    static const int DB4_ARDUINO_PIN;
+    static const int DB5_ARDUINO_PIN;
+    static const int DB6_ARDUINO_PIN;
+    static const int DB7_ARDUINO_PIN;
+    static const int RS_ARDUINO_PIN;
+    static const int E_ARDUINO_PIN;
+private:
+    ActuatorConfig();
 };
+
 
 #endif // HARDWARECONFIG_H
