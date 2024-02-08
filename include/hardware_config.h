09ae0ed8d9c339e42ec445f71d19b53bf821cfb8
Louis Cormier <louiscormier1@gmail.com>
2024-02-07 19:46:50 -0500

    avoid config class instantiation

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -10,12 +10,16 @@ public:
     static const int ACTUATOR_PIN;//todo mettre dans actuator
     static const int ANALOG_INPUT_MIN_VALUE;
     static const int ANALOG_INPUT_MAX_VALUE;
+private:
+    ArduinoConfig();
 };
 
 class DistanceSensorConfig {
 public:
     static const double MIN_READABLE_DISTANCE_MM;
     static const double MAX_READABLE_DISTANCE_MM;
+private:
+    DistanceSensorConfig();
 };
 
 class ActuatorConfig {
