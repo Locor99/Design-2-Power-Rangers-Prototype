6986cab4fc8d1d05218fea5b1e6745d48603a98e
Louis Cormier <louiscormier1@gmail.com>
2024-02-11 10:51:04 -0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
 #include "hardware_config.h"
 
 int main(){
+    LiquidCrystal lcd = LiquidCrystal(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
+                                      LcdScreenConfig::DB4_ARDUINO_PIN, LcdScreenConfig::DB5_ARDUINO_PIN,
+                                      LcdScreenConfig::DB6_ARDUINO_PIN, LcdScreenConfig:: DB7_ARDUINO_PIN);
+    Display display = Display(lcd);
     DistanceSensor distanceSensor = DistanceSensor(ArduinoConfig::SENSOR_PIN,
-                                                   )
-    Scale scale = Scale();
+                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
+                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
+    Actuator actuator = Actuator(ArduinoConfig::ACTUATOR_PIN,);
+    Scale scale = Scale(display, distanceSensor, ac);
 
     return 0;
 }
