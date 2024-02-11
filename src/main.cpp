d2ba527c964a4a0ccc41a8cb9a8772af2541cb95
Louis Cormier <louiscormier1@gmail.com>
2024-02-11 11:52:17 -0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,10 @@
 
 int main(){
     LiquidCrystal lcd = LiquidCrystal(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
-                                      LcdScreenConfig::DB4_ARDUINO_PIN, LcdScreenConfig::DB5_ARDUINO_PIN,
-                                      LcdScreenConfig::DB6_ARDUINO_PIN, LcdScreenConfig:: DB7_ARDUINO_PIN);
+                                      LcdScreenConfig::D4_ARDUINO_PIN, LcdScreenConfig::D5_ARDUINO_PIN,
+                                      LcdScreenConfig::D6_ARDUINO_PIN, LcdScreenConfig:: D7_ARDUINO_PIN);
     Display display = Display(lcd);
-    DistanceSensor distanceSensor = DistanceSensor(ArduinoConfig::SENSOR_PIN,
-                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
-                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
-    DacMCP4725 dac = DacMCP4725();
-    Actuator actuator = Actuator(dac);
-    Scale scale = Scale(display, distanceSensor, actuator);
-    scale.executeMainLoop();
+    display.displayMass(100);
 
     return 0;
 }
