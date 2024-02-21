6b6f9e2c07be16a3c6046a9acd07de7a6fe084db
Louis Cormier <louiscormier1@gmail.com>
2024-02-21 13:11:48 -0500

    le dac fonctionne

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,18 @@ const double KP = 1;
 const double KI = 0;
 const double KD = 0;
 
-int main(){
-    LiquidCrystal lcd = LiquidCrystal(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
-                                      LcdScreenConfig::D4_ARDUINO_PIN, LcdScreenConfig::D5_ARDUINO_PIN,
-                                      LcdScreenConfig::D6_ARDUINO_PIN, LcdScreenConfig:: D7_ARDUINO_PIN);
-    Display display = Display(lcd);
-    DistanceSensor distanceSensor = DistanceSensor(ArduinoConfig::DISTANCE_SENSOR_PIN,
-                                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
-                                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
-    CurrentSensor currentSensor = CurrentSensor(ArduinoConfig::CURRENT_SENSOR_PIN,
-                                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_SLOPE,
-                                                CurrentSensorConfig:: CURRENT_VS_VOLTAGE_INTERCEPT);
-    DacMCP4725 dac = DacMCP4725();
-    Actuator actuator = Actuator(dac);
+DacMCP4725 dac = DacMCP4725();
+Actuator actuator = Actuator(dac);
 
-    display.displayMass(100);
+void setup(){
+    Serial.begin(115200);
 
-    while (true){
-        actuator.setVoltage(0);
-        delay(1);
-        actuator.setVoltage(0.5);
-        delay(1);
-    }
+}
+void loop(){
+    Serial.println("test_dac");
+    actuator.setVoltage(0);
+    delay(3000);
+    actuator.setVoltage(3);
+    delay(3000);
 
-    return 0;
 }
