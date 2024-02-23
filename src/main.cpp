8ef6f12ce57459061ae43d32e91ba014bfec6cf3
Louis Cormier <louiscormier1@gmail.com>
2024-02-22 21:08:43 -0500

    replace main with loop() so arduino works

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,34 @@ const double KP = 1;
 const double KI = 0;
 const double KD = 0;
 
-int main(){
-    LiquidCrystal lcd = LiquidCrystal(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
-                                      LcdScreenConfig::D4_ARDUINO_PIN, LcdScreenConfig::D5_ARDUINO_PIN,
-                                      LcdScreenConfig::D6_ARDUINO_PIN, LcdScreenConfig:: D7_ARDUINO_PIN);
-    Display display = Display(lcd);
-    DistanceSensor distanceSensor = DistanceSensor(ArduinoConfig::DISTANCE_SENSOR_PIN,
-                                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
-                                                   DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
-    CurrentSensor currentSensor = CurrentSensor(ArduinoConfig::CURRENT_SENSOR_PIN,
-                                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_SLOPE,
-                                                CurrentSensorConfig:: CURRENT_VS_VOLTAGE_INTERCEPT);
-    DacMCP4725 dac = DacMCP4725();
-    Actuator actuator = Actuator(dac);
-    PidParameters pidParameters = PidParameters(KP, KI, KD,
-                ActuatorConfig::MIN_VOLTAGE_INPUT, ActuatorConfig::MAX_VOLTAGE_INPUT); //todo mettre kp, ki, kd ailleurs?
-    PidController pidController = PidController(pidParameters);
+LiquidCrystal lcd = LiquidCrystal(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
+                                  LcdScreenConfig::D4_ARDUINO_PIN, LcdScreenConfig::D5_ARDUINO_PIN,
+                                  LcdScreenConfig::D6_ARDUINO_PIN, LcdScreenConfig:: D7_ARDUINO_PIN);
+Display display = Display(lcd);
+DistanceSensor distanceSensor = DistanceSensor(ArduinoConfig::DISTANCE_SENSOR_PIN,
+                                               DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
+                                               DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
+CurrentSensor currentSensor = CurrentSensor(ArduinoConfig::CURRENT_SENSOR_PIN,
+                                            CurrentSensorConfig::CURRENT_VS_VOLTAGE_SLOPE,
+                                            CurrentSensorConfig:: CURRENT_VS_VOLTAGE_INTERCEPT);
+DacMCP4725 dac = DacMCP4725();
+Actuator actuator = Actuator(dac);
+PidParameters pidParameters = PidParameters(KP,
+                                            KI,
+                                            KD,
+                                            ActuatorConfig::MIN_VOLTAGE_INPUT,
+                                            ActuatorConfig::MAX_VOLTAGE_INPUT); //todo mettre kp, ki, kd ailleurs?
 
-    Scale scale = Scale(display, distanceSensor, currentSensor, actuator, pidController);
+                                            PidController pidController = PidController(pidParameters);
+
+Scale scale = Scale(display, distanceSensor, currentSensor, actuator, pidController);
+
+
+
+void setup(){
+}
+
+void loop(){
     scale.executeMainLoop();
 
-    return 0;
 }
