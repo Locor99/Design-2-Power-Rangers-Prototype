09bd8bd77c137c0a0fe469c111d613c9ce6e9c66
Louis Cormier <louiscormier1@gmail.com>
2024-02-25 14:41:59 -0500

    change config to struct

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -2,10 +2,7 @@
 #define HARDWARECONFIG_H
 #include "Arduino.h"
 
-//todo ajouter vraies valeurs
-class ArduinoConfig {
-public:
-    static const double OPERATING_VOLTAGE;
+struct ArduinoConfig {
     static const int DISTANCE_SENSOR_PIN;
     static const int CURRENT_SENSOR_PIN;
     static const int MODE_BUTTON_PIN;
@@ -13,40 +10,24 @@ public:
     static const int ANALOG_INPUT_MAX_ADC_VALUE;
     static const double ANALOG_INPUT_MIN_VOLTAGE;
     static const double ANALOG_INPUT_MAX_VOLTAGE;
-    static const int PWM_MIN_VALUE;
-    static const int PWM_MAX_VALUE;
-
-private:
-    ArduinoConfig();
-
 };
 
-class DistanceSensorConfig {
-public:
+struct DistanceSensorConfig {
     static const double DISTANCE_MM_VS_VOLTAGE_SLOPE;
     static const double DISTANCE_MM_VS_VOLTAGE_INTERCEPT;
-private:
-    DistanceSensorConfig();
 };
 
-class CurrentSensorConfig {
-public:
+struct CurrentSensorConfig {
     static const double CURRENT_VS_VOLTAGE_SLOPE;
     static const double CURRENT_VS_VOLTAGE_INTERCEPT;
-private:
-    CurrentSensorConfig();
 };
 
-class ActuatorConfig {
-public:
+struct ActuatorConfig {
     static const double MIN_VOLTAGE_INPUT;
     static const double MAX_VOLTAGE_INPUT;
-private:
-    ActuatorConfig();
 };
 
-class LcdScreenConfig{
-public:
+struct LcdScreenConfig{
     static const int D4_ARDUINO_PIN;
     static const int D5_ARDUINO_PIN;
     static const int D6_ARDUINO_PIN;
@@ -55,15 +36,10 @@ public:
     static const int E_ARDUINO_PIN;
     static const int NBR_COLUMNS;
     static const int NBR_ROWS;
-private:
-    LcdScreenConfig();
 };
 
-class ScaleConfig{
-public:
+struct ScaleConfig{
     static const double DISTANCE_SENSOR_TO_BLADE_MM;
-private:
-    ScaleConfig();
 };
 
 
--- src/hardware_config.cpp
+++ src/hardware_config.cpp
@@ -1,6 +1,5 @@
 #include "hardware_config.h"
 
-const double ArduinoConfig::OPERATING_VOLTAGE = 5.0;
 const int ArduinoConfig::DISTANCE_SENSOR_PIN = A0; // todo add real value
 const int ArduinoConfig::CURRENT_SENSOR_PIN = A1; // todo add real value
 const int ArduinoConfig::MODE_BUTTON_PIN = 22; // todo add real value
@@ -8,8 +7,6 @@ const int ArduinoConfig::ANALOG_INPUT_MIN_ADC_VALUE = 0;
 const int ArduinoConfig::ANALOG_INPUT_MAX_ADC_VALUE = 1023;
 const double ArduinoConfig::ANALOG_INPUT_MIN_VOLTAGE = 0.0;
 const double ArduinoConfig::ANALOG_INPUT_MAX_VOLTAGE = 5.0;
-const int ArduinoConfig::PWM_MIN_VALUE = 0;
-const int ArduinoConfig::PWM_MAX_VALUE = 255;
 
 const double DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE = 3; // todo add real value
 const double DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT = 0;//todo add real value
@@ -18,7 +15,7 @@ const double CurrentSensorConfig:: CURRENT_VS_VOLTAGE_SLOPE = 1;// todo add real
 const double CurrentSensorConfig:: CURRENT_VS_VOLTAGE_INTERCEPT = 0;// todo add real value
 
 const double ActuatorConfig::MIN_VOLTAGE_INPUT = 0; // todo add real value
-const double ActuatorConfig::MAX_VOLTAGE_INPUT = 5; // todo add real value
+const double ActuatorConfig::MAX_VOLTAGE_INPUT = 2.5; // todo add real value
 
 const int LcdScreenConfig::D4_ARDUINO_PIN = 26;
 const int LcdScreenConfig::D5_ARDUINO_PIN = 28;
