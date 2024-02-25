3a5544fc56cb52fb67fdd8a014d2f40f8163599c
Louis Cormier <louiscormier1@gmail.com>
2024-02-25 15:21:06 -0500

    remove pidParameters

--- src/pid_control.cpp
+++ src/pid_control.cpp
@@ -1,27 +1,16 @@
 #include "pid_control.h"
 
-PidController::PidController(const PidParameters& pidParameters)
-        : _pidParameters(pidParameters),
-          _pid(&_input, &_output, &_setpoint, pidParameters.Kp, pidParameters.Ki, pidParameters.Kd, DIRECT){
-
+PidController::PidController(double kp, double ki, double kd, bool direction)
+        : kp(kp), _pid(&_input, &_output, &setpoint, kp, ki, kd, direction){
     _pid.SetMode(AUTOMATIC);
-    _pid.SetOutputLimits(pidParameters.outputLowerBound, pidParameters.outputUpperBound);
 }
 
-void PidController::setSetpoint(double setpoint) {
-    _setpoint = setpoint;
-}
-
-void PidController::setInput(double input) {
-    _input = input;
-}
 
 double PidController::computeOutput() {
     _pid.Compute();
     return _output;
 }
 
-void PidController::updateParameters(const PidParameters& parameters) {
-    _pidParameters = parameters;
-    _pid.SetTunings(_pidParameters.Kp, _pidParameters.Ki, _pidParameters.Kd);
+void PidController::setOutputLimits(double min, double max) {
+    _pid.SetOutputLimits(min, max);
 }
