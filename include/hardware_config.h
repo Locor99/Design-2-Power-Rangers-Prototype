b4785b639323a213a422bc56daa76923a80b4a19
Louis Cormier <louiscormier1@gmail.com>
2024-02-25 15:29:51 -0500

    change config to struct

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -3,46 +3,43 @@
 #include "Arduino.h"
 
 struct ArduinoConfig {
-    static const int DISTANCE_SENSOR_PIN;
-    static const int CURRENT_SENSOR_PIN;
-    static const int MODE_BUTTON_PIN;
-    static const int ANALOG_INPUT_MIN_ADC_VALUE;
-    static const int ANALOG_INPUT_MAX_ADC_VALUE;
-    static const double ANALOG_INPUT_MIN_VOLTAGE;
-    static const double ANALOG_INPUT_MAX_VOLTAGE;
+    static const int DISTANCE_SENSOR_PIN = A0;  // todo add real value
+    static const int CURRENT_SENSOR_PIN = A1;  // todo add real value
+    static const int MODE_BUTTON_PIN = 22;  // todo add real value
+    static const int ANALOG_INPUT_MIN_ADC_VALUE = 0;
+    static const int ANALOG_INPUT_MAX_ADC_VALUE = 1023;
+    constexpr static const double ANALOG_INPUT_MIN_VOLTAGE = 0.0;
+    constexpr static const double ANALOG_INPUT_MAX_VOLTAGE = 5.0;
 };
 
 struct DistanceSensorConfig {
-    static const double DISTANCE_MM_VS_VOLTAGE_SLOPE;
-    static const double DISTANCE_MM_VS_VOLTAGE_INTERCEPT;
+    constexpr static const double DISTANCE_MM_VS_VOLTAGE_SLOPE = 3;  // todo add real value
+    constexpr static const double DISTANCE_MM_VS_VOLTAGE_INTERCEPT = 0;  //todo add real value
 };
 
 struct CurrentSensorConfig {
-    static const double CURRENT_VS_VOLTAGE_SLOPE;
-    static const double CURRENT_VS_VOLTAGE_INTERCEPT;
+    constexpr static const double CURRENT_VS_VOLTAGE_SLOPE = 1;  // todo add real value
+    constexpr static const double CURRENT_VS_VOLTAGE_INTERCEPT = 0;  // todo add real value
 };
 
 struct ActuatorConfig {
-    static const double MIN_VOLTAGE_INPUT;
-    static const double MAX_VOLTAGE_INPUT;
+    constexpr static const double MIN_VOLTAGE_INPUT = 0;  // todo add real value
+    constexpr static const double MAX_VOLTAGE_INPUT = 2.5;  // todo add real value
 };
 
 struct LcdScreenConfig{
-    static const int D4_ARDUINO_PIN;
-    static const int D5_ARDUINO_PIN;
-    static const int D6_ARDUINO_PIN;
-    static const int D7_ARDUINO_PIN;
-    static const int RS_ARDUINO_PIN;
-    static const int E_ARDUINO_PIN;
-    static const int NBR_COLUMNS;
-    static const int NBR_ROWS;
+    static const int D4_ARDUINO_PIN = 26;
+    static const int D5_ARDUINO_PIN = 28;
+    static const int D6_ARDUINO_PIN = 30;
+    static const int D7_ARDUINO_PIN = 32;
+    static const int RS_ARDUINO_PIN = 22;
+    static const int E_ARDUINO_PIN = 24;
+    static const int NBR_COLUMNS = 16;
+    static const int NBR_ROWS = 2;
 };
 
 struct ScaleConfig{
-    static const double DISTANCE_SENSOR_TO_BLADE_MM;
+    constexpr static const double DISTANCE_SENSOR_TO_BLADE_MM = 50;  // todo add real value
 };
-
-
-
 
 #endif // HARDWARECONFIG_H
