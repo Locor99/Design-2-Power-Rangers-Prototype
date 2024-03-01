005ee06efa5f3b76efa487796cc27d0287ff3a52
Louis Cormier <louiscormier1@gmail.com>
2024-03-01 17:30:36 -0500

    tiny little display improvement

--- src/display.cpp
+++ src/display.cpp
@@ -4,10 +4,8 @@ Display::Display(LiquidCrystal &lcd): _lcd(lcd) {
     _lcd.begin(LcdScreenConfig::NBR_COLUMNS, LcdScreenConfig::NBR_ROWS);
 }
 
-void Display::displayMass(double mass) {
-    _lcd.setCursor(0,0);
-    _lcd.print(mass,2);
 void Display::displayMass(double massGrams) {
+    _lcd.home();
     _lcd.print(massGrams, 2);
     _lcd.print(" g");
 }
