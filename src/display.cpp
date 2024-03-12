281cd9242a211f5fa5167bbdc7253fb0bfa038fc
Louis Cormier <louiscormier1@gmail.com>
2024-03-11 20:04:09 -0400

    decrease display precision

--- src/display.cpp
+++ src/display.cpp
@@ -2,11 +2,12 @@
 
 Display::Display(LiquidCrystal &lcd): _lcd(lcd) {
     _lcd.begin(LcdScreenConfig::NBR_COLUMNS, LcdScreenConfig::NBR_ROWS);
+    _lcd.clear();
 }
 
 void Display::displayMass(double massGrams) {
     _lcd.home();
-    _lcd.print(massGrams, 2);
+    _lcd.print(massGrams, 1);
     _lcd.print(" g");
 }
 
