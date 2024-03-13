9a38274e9e537d087c7626d0393be753a6cda68f
Louis Cormier <louiscormier1@gmail.com>
2024-03-13 18:00:47 -0400

    remove filter constant setup

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,6 @@ void setup() {
     pidController.setpoint = ScaleConfig::DISTANCE_OF_BLADE_SETPOINT_MM;
 
     distanceSensor.setFilterConstant(0.75); //remove filter if not necessary
-    currentSensor.setFilterConstant(0.75); //remove filter if not necessary
 
 
     Scale scale(display,
