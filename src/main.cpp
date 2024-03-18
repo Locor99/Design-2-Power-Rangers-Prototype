10d2b7c63cc75eb247715a2f8f4de5e40116f8b5
Louis Cormier <louiscormier1@gmail.com>
2024-03-18 14:41:22 -0400

    vider main pour tester

--- src/main.cpp
+++ src/main.cpp
@@ -18,38 +18,10 @@ const double CURRENT_PID_KD= 0;
 
 void setup() {
     Serial.begin(115200);
-    LiquidCrystal lcd(LcdScreenConfig::RS_ARDUINO_PIN, LcdScreenConfig::E_ARDUINO_PIN,
-                      LcdScreenConfig::D4_ARDUINO_PIN, LcdScreenConfig::D5_ARDUINO_PIN,
-                      LcdScreenConfig::D6_ARDUINO_PIN, LcdScreenConfig::D7_ARDUINO_PIN);
-    UserInterface display(lcd, LcdScreenConfig::NBR_ROWS, LcdScreenConfig::NBR_COLUMNS);
-    DistanceSensor distanceSensor(ArduinoConfig::DISTANCE_SENSOR_PIN,
-                                  DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_SLOPE,
-                                  DistanceSensorConfig::DISTANCE_MM_VS_VOLTAGE_INTERCEPT);
-    CurrentSensor currentSensor(ArduinoConfig::CURRENT_SENSOR_PIN,
-                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_SLOPE,
-                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_INTERCEPT);
-    DacMCP4725 dac;
-    Actuator actuator(dac);
-
-    PidController positionRegulator(POSITION_PID_KP, POSITION_PID_KI, POSITION_PID_KD, DIRECT);
-    positionRegulator.setOutputLimits(ActuatorConfig::MIN_CURRENT_INPUT_ON_AMPLI, ActuatorConfig::MAX_CURRENT_INPUT_ON_AMPLI);
-    positionRegulator.setpoint = ScaleConfig::DISTANCE_OF_BLADE_SETPOINT_MM;
-
     PidController currentRegulator(CURRENT_PID_KP, CURRENT_PID_KI, CURRENT_PID_KD, DIRECT);
     currentRegulator.setOutputLimits(ActuatorConfig::MIN_VOLTAGE_INPUT_ON_AMPLI, ActuatorConfig::MAX_VOLTAGE_INPUT_ON_AMPLI);
 
-    distanceSensor.setFilterConstant(0.75); //remove filter if not necessary
-    currentSensor.setFilterConstant(1); //remove filter if not necessary
+   while(true){
 
-
-    Scale scale(display,
-                distanceSensor,
-                currentSensor,
-                actuator,
-                positionRegulator,
-                currentRegulator,
-                ScaleConfig::FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_SLOPE,
-                ScaleConfig::FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_INTERCEPT);
-
-    scale.executeMainLoop();
+   }
 }
