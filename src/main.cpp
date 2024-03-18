734aed67a081286a5539c73b7040e0921945eca1
Louis Cormier <louiscormier1@gmail.com>
2024-03-18 15:32:38 -0400

    reguler

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,44 @@ const double POSITION_PID_KP = 0.5;
 const double POSITION_PID_KI = 0;
 const double POSITION_PID_KD = 0;
 
-const double CURRENT_PID_KP = 0.1;
+const double CURRENT_PID_KP = 0.5;
 const double CURRENT_PID_KI = 0;
 const double CURRENT_PID_KD= 0;
 
+void regulate(CurrentSensor& currentSensor,
+              PidController& currentRegulator,
+              DacMCP4725 &dac){
+    double current = currentSensor.getCurrent();
+    currentRegulator.input = current;
+    Serial.println("courant:");
+    Serial.println(current);
+
+    double voltageToDac = currentRegulator.computeOutput();
+    dac.setOutputVoltage(voltageToDac);
+    Serial.println("Tension dac:");
+    Serial.println(voltageToDac);
+}
+
 void setup() {
     Serial.begin(115200);
+    DacMCP4725 dac;
     PidController currentRegulator(CURRENT_PID_KP, CURRENT_PID_KI, CURRENT_PID_KD, DIRECT);
     currentRegulator.setOutputLimits(ActuatorConfig::MIN_VOLTAGE_INPUT_ON_AMPLI, ActuatorConfig::MAX_VOLTAGE_INPUT_ON_AMPLI);
+    CurrentSensor currentSensor(ArduinoConfig::CURRENT_SENSOR_PIN,
+                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_SLOPE,
+                                CurrentSensorConfig::CURRENT_VS_VOLTAGE_INTERCEPT);
 
-   while(true){
+    const long time = 3000;
 
-   }
+    while(true){
+
+        currentRegulator.setpoint = 1;
+        regulate(currentSensor, currentRegulator, dac);
+        delay(time);
+
+        currentRegulator.setpoint = 2;
+        regulate(currentSensor, currentRegulator, dac);
+        delay(time);
+    }
 }
+
