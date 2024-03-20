c725662d2ee4ea9ae9945a7c939e910c188fac2f
Louis Cormier <louiscormier1@gmail.com>
2024-03-20 15:04:05 -0400

    add units enum

---
+++ include/units.h
@@ -0,0 +1,13 @@
+//
+// Created by louis on 2024-03-20.
+//
+
+#ifndef DESIGN2_PROTOTYPE_UNITS_H
+#define DESIGN2_PROTOTYPE_UNITS_H
+
+enum class Units {
+    GRAMS,
+    OUNCES
+};
+
+#endif //DESIGN2_PROTOTYPE_UNITS_H
