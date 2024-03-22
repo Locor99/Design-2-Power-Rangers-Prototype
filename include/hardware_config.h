31ee4fecfc56820dd0f9105e84e7e1014cf09354
Louis Cormier <louiscormier1@gmail.com>
2024-03-22 15:25:50 -0400

    Ajustement calibration (#18)
    * add semi-adaptable filtering
    
    * changer la calibration

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -44,8 +44,8 @@ struct LcdScreenConfig{
 
 struct ScaleConfig{
     constexpr static const double DISTANCE_OF_BLADE_SETPOINT_MM = 15;
-    constexpr static const double FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_SLOPE = 92.83;
-    constexpr static const double FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_INTERCEPT = -19.60;
+    constexpr static const double FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_SLOPE = 98.90;
+    constexpr static const double FORCE_APPLIED_BY_ACTUATOR_N_VS_CURRENT_A_INTERCEPT = -17.94;
 
 };
 
